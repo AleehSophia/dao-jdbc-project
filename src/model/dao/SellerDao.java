6f382984722098ce29cfecd30a22e3fe73b96443
AleehSophia <alessia.casado@gmail.com>
2022-10-31 10:21:11 -0300

    :sparkles:feat: findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -9,6 +9,6 @@ public interface SellerDao {
     void insert(Seller obj);
     void update(Seller obj);
     void deleteById(Seller id);
-    Seller findById(Seller id);
+    Seller findById(Integer id);
     List<Seller> findAll();
 }
